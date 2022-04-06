e9285c7433e473d6c20cba90c599b5fb052bde53
mati-quintana-udesa <quintanam@udesa.edu.ar>
2022-04-06 01:40:11 -0500

    Primera modificacion mati, creee los archivos primcipales (algunos)

---
+++ Programación2_DH/trabajoIntegradorDos/data/info.js
@@ -0,0 +1,22 @@
+const data = {
+    lista: [
+        {
+            id: 1,
+            nombre: "ejemplo1",
+        },
+
+        {
+            id: 2,
+            nombre: "ejemplo2",
+        },
+
+
+    ]
+};
+
+
+
+
+
+
+module.exports(data);
