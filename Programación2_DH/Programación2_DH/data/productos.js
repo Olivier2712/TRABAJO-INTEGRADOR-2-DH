afed83fb53671f935757a1efe2f183617a1e9532
Olivier2712 <olegras@udesa.edu.ar>
2022-05-11 08:30:44 -0300

    Añadi sequelize y edite sus respectivas carpetas

---
+++ Programación2_DH/Programación2_DH/data/productos.js
@@ -0,0 +1,53 @@
+
+
+
+
+const productos = {
+    lista: [
+        {
+            id: 1,
+            modelo: "jordan 1 ",
+        },
+
+        {
+            id: 2,
+            nombre: "Jordan x Off-White",
+        },
+        {
+            id: 3,
+            nombre: "Yeezy 700",
+        },
+        {
+            id: 4,
+            nombre: "Nike blazers",
+        },
+        {
+            id: 5,
+            nombre: "Human Race black",
+        },
+        {
+            id: 6,
+            nombre: "Jordan Retro",
+        },
+        {
+            id: 7,
+            nombre: "AirForce 1",
+        },
+        {
+            id: 8,
+            nombre: "Off-White vulcano",
+        },
+        {
+            id: 9,
+            nombre: "Nike air force max 1",
+        },
+        {
+            id: 10,
+            nombre: "Air Jordan 4 retro ",
+        },
+
+
+    ]
+};
+
+module.exports = productos;
