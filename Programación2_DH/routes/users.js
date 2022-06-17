a93a622c9a87c8b9cd47ec49a6d1d58f608350c0
Olivier2712 <olegras@udesa.com>
2022-06-17 10:56:41 -0300

    Agregue una ruta POST para el registro de usuario

--- Programación2_DH/routes/users.js
+++ Programación2_DH/routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var userController = require("../controladores/userController")
 
 router.get('/register', userController.registro);
+router.post('/register', userController.almacenarRegistro);
 router.get('/login', userController.login);
 router.get('/perfil', userController.profile);
 router.get('/editar-perfil', userController.editProfile);
