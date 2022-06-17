00011777a62cbff86fa0e2af30c0c8b418e25825
Olivier2712 <olegras@udesa.com>
2022-06-17 10:55:33 -0300

    Modifique el userControllolers.js

--- Programación2_DH/controladores/userController.js
+++ Programación2_DH/controladores/userController.js
@@ -1,19 +1,45 @@
 const productos = require("../data/productos")
 const usuarios =require("../data/usuario") 
-const db = require('../database/models');
+const db = require('../database/models/index');
+const bcrypt = require('bcryptjs');
 
 const userController = {
     
-    registro: function(req,res){res.render('register')},
+    registro: function(req,res) {
+        res.render('register')
+    },
 
-    login: function(req,res){res.render('login')},
+    almacenarRegistro: async function(req,res) {
 
-    profile: function(req,res){res.render('profile',{producto: productos, usuario: usuarios, logueado: true})},
+        const nuevoUsuario = {
+            nombre_de_usuario: req.body.nombre,
+            email: req.body.email,
+            contrasenia: bcrypt.hashSync(req.body.password),
+            fecha_de_nacimiento: req.body.fechaNacimiento,
+            update_at: new Date(),
+            create_at: new Date()
+        }
+
+        await db.Usuario.create(nuevoUsuario);
+        res.redirect('/users/login');
+    },
+
+    login: function(req,res) {
+        res.render('login')
+    },
+
+    profile: function(req,res) {
+        res.render(
+            'profile',
+            { producto: productos, usuario: usuarios, logueado: true}
+        )
+    },
     
-     editProfile: function(req,res){
-    res.render('profile-edit', { productos: productos.usuario, usuario: usuarios, logueado: true})
-        
-   
+    editProfile: function(req,res) {
+        res.render(
+            'profile-edit', 
+            { productos: productos.usuario, usuario: usuarios, logueado: true}
+        )
     },
 
 
